efd997d78ae1d6979ce8b466dc89848f8c454558
xw <23066125+xwang1000@users.noreply.github.com>
2019-03-19 23:03:26 -0700

    Major style change and data entry

---
+++ data.js
@@ -0,0 +1,16 @@
+export function data(){
+  return [
+    {
+      name: 'LearningHub',
+      description: 'a single-page application for learners to save and share resources, also interacting with other users’ posts. I was responsible for the frontend, api and database.',
+      time: 'Spring, 2019',
+      quote: 'I built it with HTML5, CSS3, Sass, BootStrap, Javascript, AJAX, Node, Express, EJS, and MongoDB'
+    },
+    {
+      name: 'O5 Tea Bar Menu',
+      description: 'a single-page application that allows customers to browse micro-farme rare tea and explore its origins. The database connects to Airtable, allows staff to easily update the menu without any tech or graphic design background.',
+      time: 'Spring, 2019',
+      quote: 'I was in charge of the initial front-end development, database setup, and client relationship. I used Vue.js, HTML5/SASS, Javascript, and Airtable API.'
+    }
+  ]
+}
---
+++ index.js
@@ -0,0 +1,15 @@
+// import {data}
+// document.addEventListener("DOMContentLoaded", function(event) {
+//   'strict'
+
+
+//   console.log('loaded')
+//   console.log(data)
+// });
+
+(function() {
+  // import data from 'data'
+
+  console.log('loaded')
+  // console.log(data)
+})()
