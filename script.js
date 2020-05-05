66e1ab486a32ebcb7aea87a8788b29b9056913e3
xwang1000 <23066125+xwang1000@users.noreply.github.com>
2020-05-05 15:26:14 -0700

    Update content

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // Scrolling animation
-// const SCROLLING_TIME = 800;
-// $(document).on('click', 'a', function (event) {
-//   $('html, body').animate({
-//      scrollTop:$(jQuery.attr(this, 'href')).offset().top
-//    }, SCROLLING_TIME);
-//    return false;
-//  });
+const SCROLLING_TIME = 800;
+$(document).on('click', 'a', function (event) {
+  $('html, body').animate({
+     scrollTop:$(jQuery.attr(this, 'href')).offset().top
+   }, SCROLLING_TIME);
+   return false;
+ });
 
 $(function() {
   AOS.init();
